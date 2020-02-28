807a66f5e28887d35992a0adc2cacf3bf664f43c
BrooksPoltl <bpoltl1@gmail.com>
2020-02-28 05:22:35 -0600

    creating local file with template

--- templates/main.go
+++ templates/main.go
@@ -1,20 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+	"text/template"
+)
 
 func main() {
-	name := "Brooks Poltl"
-	tpl := `
-		 <!DOCTYPE html>
-		 <html lang="en">
-		 <head>
-		 <meta charset="UTF-8">
-		 <title>Hello World!</title>
-		 </head>
-		 <body>
-		 <h1>` + name + `</h1>
-		 </body>
-		 </html>
-		 `
-	fmt.Println(tpl)
+	tpl, err := template.ParseFiles("tpl.gohtml")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	nf, err := os.Create("index.html")
+	if err != nil {
+		log.Println("error creating file", err)
+	}
+	defer nf.Close()
+
+	err = tpl.Execute(nf, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
